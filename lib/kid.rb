9a98774ddc50a707495d2b0dd7bf53fd3f621495
github username <github email address>
2020-05-18 16:30:24 -0600

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
